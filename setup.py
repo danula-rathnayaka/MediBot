5825134054f55f50c91f8ab3a28e46444ba133f3
Danula <danusathmin@gmail.com>
2025-03-19 21:32:17 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MediBot',
+    version= '0.0.0',
+    author= 'Danula Rathnayaka',
+    author_email= 'danusathmin@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
